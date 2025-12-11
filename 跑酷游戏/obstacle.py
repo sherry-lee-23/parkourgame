881fae6b2404a4348db8dd71f4e42aa47678907d
sherry-lee-23 <zhangqiming061218@outlook.com>
2025-12-11 19:53:13 +0800

    Add files via upload

--- 跑酷游戏/obstacle.py
+++ 跑酷游戏/obstacle.py
@@ -60,8 +60,8 @@ class ObstacleManager:
     def spawn_obstacle(self):
         """生成一个新的障碍物"""
         # 随机高度和宽度
-        obstacle_height = random.randint(25, 60)
-        obstacle_width = random.randint(25, 40)#更改了高度和宽度
+        obstacle_height = random.randint(40, 90)
+        obstacle_width = random.randint(40, 90)#更改了高度和宽度
         obstacle_y = 400 - obstacle_height  # 底部在地面上，地面为400
 
         # 障碍物速度
